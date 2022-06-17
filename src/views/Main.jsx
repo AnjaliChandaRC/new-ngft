20ad9ae5a531ece462b601e15f9434649b6678f9
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2022-06-17 11:09:13 +0530

    first commit

---
+++ src/views/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Header from "../components/Header";
+function Main() {
+  return (
+    <>
+      <Hero />
+      <Header />
+    </>
+  );
+}
+
+export default Main;
